cd3ab61976ffdea1e4fc57194bf23d02d12b43d8
KartikPandey19 <kartik2000pandey@gmail.com>
2021-05-02 15:04:31 +0530

    V1

--- Activity/script.js
+++ Activity/script.js
@@ -1,37 +1,45 @@
 let colorBtn = document.querySelectorAll(".filter_color");
 let mainContainer = document.querySelector(".main_container");
+let bothElementsArr = document.querySelectorAll(".icon-container");
+let crossBtn = bothElementsArr[1]
+let plusButton = bothElementsArr[0];
 let body = document.body;
-let plusButton = document.querySelector(".fa-plus");
-
-
-for (let i = 0; i < colorBtn.length; i++) {
-    colorBtn[i].addEventListener("click", function (e) {
-        let color = colorBtn[i].classList[1];
-        mainContainer.style.backgroundColor = color;
-    })
+let deleteState = false;
+let taskArr = [];
+if (localStorage.getItem("allTask")) {
+    taskArr = JSON.parse(localStorage.getItem("allTask"));
+    // UI
+    for (let i = 0; i < taskArr.length; i++) {
+        let { id, color, task } = taskArr[i];
+        createTask(color, task, false, id);
+    }
 }
 plusButton.addEventListener("click", createModal);
+crossBtn.addEventListener("click", setDeleteState);
 function createModal() {
     // create modal
-    let modal_container = document.createElement("div");
-    modal_container.setAttribute("class", "modal_container");
-    modal_container.innerHTML = `<div class="input_container">
-    <textarea class="modal_input" 
-    placeholder="Enter Your text"></textarea>
-</div>
-<div class="modal_filter_container">
-    <div class="filter pink"></div>
-    <div class="filter blue"></div>
-    <div class="filter green"></div>
-    <div class="filter black"></div>
-</div>`;
-    body.appendChild(modal_container);
+    let modalContainer = document.querySelector(".modal_container");
+    if (modalContainer == null) {
+        modalContainer = document.createElement("div");
+        modalContainer.setAttribute("class", "modal_container");
+        modalContainer.innerHTML = `<div class="input_container">
+        <textarea class="modal_input" 
+        placeholder="Enter Your text"></textarea>
+    </div>
+    <div class="modal_filter_container">
+        <div class="filter pink"></div>
+        <div class="filter blue"></div>
+        <div class="filter green"></div>
+        <div class="filter black"></div>
+    </div>`;
+        body.appendChild(modalContainer);
+        handleModal(modalContainer);
+    }
+    let textarea = modalContainer.querySelector(".modal_input");
+    textarea.value = "";
+
     //  event listner 
-    handleModal(modal_container);
-
-
 }
-
 function handleModal(modal_container) {
     let cColor = "black";
     let modalFilters = document.querySelectorAll(".modal_filter_container .filter");
@@ -51,30 +59,109 @@ function handleModal(modal_container) {
             //  color 
             cColor = modalFilters[i].classList[1];
             console.log("current color of task", cColor);
-
         })
     }
     let textArea = document.querySelector(".modal_input");
     textArea.addEventListener("keydown", function (e) {
-        if (e.key == "Enter"&&textArea.value!="") {
+        if (e.key == "Enter" && textArea.value != "") {
             console.log("Task ", textArea.value, "color ", cColor);
             //  remove modal
             modal_container.remove();
             // create taskBox
-            createTask(cColor, textArea.value);
+            createTask(cColor, textArea.value, true);
+
         }
     })
 
 
 }
-function createTask(color, task) {
+function createTask(color, task, flag, id) {
+    // color area click-> among colors
     let taskContainer = document.createElement("div");
+
+    let uifn = new ShortUniqueId();
+    let uid = id || uifn();
     taskContainer.setAttribute("class", "task_container");
     taskContainer.innerHTML = `<div class="task_filter ${color}"></div>
     <div class="task_desc_container">
-        <h3 class="uid">#example</h3>
-        <div class="task_desc">${task}</div>
+        <h3 class="uid">#${uid}</h3>
+        <div class="task_desc" contenteditable="true" >${task}</div>
     </div>
 </div >`;
     mainContainer.appendChild(taskContainer);
+    let taskFilter = taskContainer.querySelector(".task_filter");
+    if (flag == true) {
+        // console.log(uid);
+        let obj = { "task": task, "id": `${uid}`, "color": color };
+        taskArr.push(obj);
+        let finalArr = JSON.stringify(taskArr);
+        localStorage.setItem("allTask", finalArr);
+    }
+    taskFilter.addEventListener("click", changeColor);
+    taskContainer.addEventListener("click", deleteTask);
+    let taskDesc = taskContainer.querySelector(".task_desc");
+    taskDesc.addEventListener("keypress", editTask);
+
+}
+function changeColor(e) {
+    //  add event listener 
+    // console.log(e.currentTarget);
+    // /event occur 
+    // console.log(e.target);
+    let taskFilter = e.currentTarget;
+    let colors = ["pink", "blue", "green", "black"];
+    let cColor = taskFilter.classList[1];
+    let idx = colors.indexOf(cColor);
+    let newColorIdx = (idx + 1) % 4;
+    taskFilter.classList.remove(cColor);
+    taskFilter.classList.add(colors[newColorIdx]);
+}
+function setDeleteState(e) {
+
+    let crossBtn = e.currentTarget;
+    // console.log(crossBtn.parent)
+    if (deleteState == false) {
+        crossBtn.classList.add("active");
+    } else {
+        crossBtn.classList.remove("active");
+    }
+    deleteState = !deleteState;
+}
+function deleteTask(e) {
+    let taskContainer = e.currentTarget;
+    if (deleteState) {
+        // local storage search -> remove
+        let uidElem = taskContainer.querySelector(".uid");
+        let uid = uidElem.innerText.split("#")[1];
+        for (let i = 0; i < taskArr.length; i++) {
+            let { id } = taskArr[i];
+            console.log(id, uid);
+            if (id == uid) {
+                taskArr.splice(i, 1);
+                let finalTaskArr = JSON.stringify(taskArr);
+                localStorage.setItem("allTask", finalTaskArr);
+                taskContainer.remove();
+                break;
+            }
+        }
+    }
+}
+
+
+
+function editTask(e) {
+    let taskDesc = e.currentTarget;
+    let uidElem = taskDesc.parentNode.children[0];
+    let uid = uidElem.innerText.split("#")[1];
+    for (let i = 0; i < taskArr.length; i++) {
+        let { id } = taskArr[i];
+        console.log(id, uid);
+        if (id == uid) {
+            taskArr[i].task = taskDesc.innerText
+            let finalTaskArr = JSON.stringify(taskArr);
+            localStorage.setItem("allTask", finalTaskArr);
+
+            break;
+        }
+    }
 }
